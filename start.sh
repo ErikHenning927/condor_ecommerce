68064f4f16ae4559ae41f5e6f63ec67dde990f76
Erik Henning <henningerik2@gmail.com>
2023-11-29 12:33:40 -0300

    Socket

--- start.sh
+++ start.sh
@@ -3,8 +3,7 @@ pip install -r requirements.txt
 # Instalar dependências
 
 # Executar o aplicativo Streamlit
-streamlit run home.py --server.port=16549 --server.address=0.0.0.0 --server.enableCORS=false --server.enableWebsocketCompression=false
-
+streamlit run home.py --server.enableWebsocketCompression=false
 
 
 
