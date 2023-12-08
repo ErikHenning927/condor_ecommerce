3bbcb4e0f5574ccab456caaba6c9fe9c1293ff6b
Erik Henning <henningerik2@gmail.com>
2023-12-08 10:27:47 -0300

    Carga Preços

--- markets/condoremcasa.py
+++ markets/condoremcasa.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 def condoremcasa():
-    file = 'dataset/site-mercado-loja-21-20231101050956.csv'
+    file = 'dataset/site-mercado-loja-21-20231208040928.csv'
     df = pd.read_csv(file, delimiter=';')
     df = df.dropna(axis=1, how='all')
     df.to_json('json/cec.json', orient='records', lines=False)
@@ -20,3 +20,5 @@ def condoremcasa():
         })
     with open('json/condoremcasa.json', 'w') as file:
         json.dump(json_structure, file, ensure_ascii=False, indent=4)
+
+condoremcasa()
